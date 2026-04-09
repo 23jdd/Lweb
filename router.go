d83bb147bc5c0b4a7a82bab0b7b66e6be185f329
long <15096050739@163.com>
2026-04-09 10:47:25 +0800

    display

--- router.go
+++ router.go
@@ -583,6 +583,7 @@ func (e *Engine) hasPath(path string) bool {
 	return false
 }
 func (e *Engine) Run(addr string) error {
+	e.T.Display()
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		context := e.pool.Get().(*Context)
 		defer e.pool.Put(context)
