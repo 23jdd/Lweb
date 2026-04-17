17d6ab7468040a8ff9f067a1759b62714b06113b
jjj <15096059739@163.com>
2026-04-17 18:32:35 +0800

    2

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 		})
 	})
 	fmt.Printf("Server start...%d\n", 8080)
-	fmt.Println("hello world")
+	fmt.Println("hello world2")
 	if err := engine.Run(":8080"); err != nil {
 		panic(err)
 	}
